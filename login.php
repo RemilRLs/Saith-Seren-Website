bb4807b3b9b5e7f0af7fdec3ae738290ed6192eb
Remil <51848204+RemilRLs@users.noreply.github.com>
2023-01-05 09:46:09 +0100

    creation of a login page

---
+++ login.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset = "UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Saith Seren / Home</title> 
+    <link rel="shortcut icon" type="image/png" href="ressources/logo/Saith Seren Logo.png"/>
+    
+    <link rel="stylesheet" type="text/css" href="css/navbar.css"/>
+    <link rel="stylesheet" type="text/css" href="css/login.css"/>
+
+    <script src="js/navbar.js" defer></script>
+
+
+</head>
+
+
+<body>
+<header class="primary-header flex">
+        <nav data-visible="false" class="navbar">
+            <div class="max-width">
+                <div class="logo">
+                    <img src="ressources/logo/Saith Seren Logo.png">
+                </div>
+                
+                <!-- Element of the navbar. -->
+                <button class="mobile-nav-toggle" aria-controls="menu-navigation" aria-expanded="false"><span class="sr-only" ></span></button>
+                <ul data-visible="false" class="menu-navigation flex">
+
+                        
+                        <div class="menu-navigation-left">
+                            <li><a class="navbar-element" href="#about">Home</a></li>
+                            <li><a class="navbar-element margin" href="calendar.html">Calendar</a></li>
+                            <li><a class="navbar-element" href="#pictures">Pictures</a></li>
+                            <li><a class="navbar-element margin" href="#">The Centre</a></li>
+                            <li><a class="navbar-element" href="#contact">Contact</a></li>
+                        </div>
+
+                        <div  class="menu-navigation-right">
+                            <li><a class="navbar-element-right subscribe" href="#">Subscribe</a></li>
+
+                            <!-- Section for choosing a language. -->
+
+                            <div class="logo">
+
+                            </div>
+
+                            <div class="lang-menu">
+                                <div class="selected-lang">
+                                    <img src="ressources/logo/united-kingdom.png">
+                                    <a>Welsh</a>
+                                </div>
+
+                                <ul>
+                                    <li><a class="wl"><img src="ressources/logo/united-kingdom.png">Welsh</a></li>
+                                    <li><a class="en"><img src="ressources/logo/united-kingdom.png">English</a></li>
+
+                                </ul>   
+                            </div>
+    
+                        </div>
+                    
+
+
+                </ul>
+            </div>
+        </nav>
+
+    </header>
+
+</body>
